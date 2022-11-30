ce25ebce9e157ec629d2b6b6a0908ef4f502eec7
Kesavan Rangan <68580746+kesavan-rangan@users.noreply.github.com>
2022-11-30 18:54:39 +0530

    Adding intermediate prints

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from flask import request
+import traceback
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/interactive', methods=['POST'])
+def slack_interactivity():
+    try:
+        print("-------Request ARGS -------")
+        print(request.args)
+        print("-------Request Headers -------")
+        print(request.headers)
+        print("-------Request Form -------")
+        print(request.form)
+        print("-------Request JSON body -------")
+        print(request.get_json())
+        print("-------Done-------")
+    except Exception as e:
+        message = traceback.format_exc()
+        print(message)
+    return 'slack interactivity'
